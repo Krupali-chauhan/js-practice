2d1845379685bb0ab790fcf1d1e5044529de899b
Krupali-chauhan <krupichauhan@gmail.com>
2026-08-08 07:23:07 +0000

    for dattype and conversion

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"//for use newere varsion of javascript now a days we are not use because type=module it ay=utomatically consider as a newer version of javascript
+
+let score=33  // number
+let name="Krupali"// string
+let isLoggedIn=true// boolean
+
+// In javascript there are two type of datatype
+//1. primitive    2.non-primitive
+//1 .primitive
+//  - number : it store int and flot/double value  . in javascript not separate float type
+//  -bigInt  :2 powe 53  like trading stock market values
+//  -string :it strore string value. that cover in like '',"" qoutes
+//   -bollean: true/false
+//  -null   : standalone value //let x=null
+//                              console.log(typeof(x))
+//                              //output : object 
+//           that mean null is object type
+// - undefind : standalone value // type undefine
+//null vs undefind       null mean devloper intensly assign null  value 
+//                      undefind mean not assifgn value je javascript jate assign kare
+//symbol: unique value 
+// 2. non-primitive
+//-object
+//-array
+//-function 
